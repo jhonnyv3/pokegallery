2de7ade976ffe1c971dc3c23b4a22827d3871446
Jhonny Vasquez <jhonny.vasquez@wom.cl>
2025-05-10 23:19:10 -0400

    chore: agregar paths personalizados para imports absolutos en core, shared y features

--- src/app/features/pokemon/components/pokemon-list/pokemon-list.component.ts
+++ src/app/features/pokemon/components/pokemon-list/pokemon-list.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonDetail } from '@features/pokemon/models/pokemon.model';
+import { PokeEndpoints } from '@core/constants/poke-endpoints';
 
 @Component({
+  standalone: true,
   selector: 'app-pokemon-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pokemon-list.component.html',
-  styleUrl: './pokemon-list.component.scss'
+  styleUrls: ['./pokemon-list.component.scss']
 })
 export class PokemonListComponent {
+  @Input() pokemons: PokemonDetail[] = [];
 
+  spriteUrl(id: number): string {
+    return PokeEndpoints.image(id);
+  }
 }
